626ce572878ed4f5162d74907f2a34981c16bb37
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-09-20 17:55:51 +0600

    data types in ts

---
+++ basic-types/index.js
@@ -0,0 +1,7 @@
+//built in types: number, string, boolean, void, undefined, null
+var userId;
+var name = "Mahbub";
+userId = 121;
+// userId = "Mahbub"; will not work
+var isTrue = true;
+var isFalse = "true"; //wont work
---
+++ basic-types/index.ts
@@ -0,0 +1,9 @@
+//built in types: number, string, boolean, void, undefined, null
+
+let userId:number;
+let name:string = "Mahbub";
+userId = 121;
+// userId = "Mahbub"; will not work
+
+let isTrue:boolean = true;
+let isFalse:boolean = "true";   //wont work
