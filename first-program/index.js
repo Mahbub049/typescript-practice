7f514bcc13ba148e5e4ed3cf627feeb16838cdeb
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-09-20 17:36:41 +0600

    first program ts

---
+++ first-program/index.js
@@ -0,0 +1,12 @@
+// BASICS: First Program
+// use tsc index.ts --watch to auto convert or tsc index.ts into index.js and then node index.js
+var userName = "Mahbub Sarwar";
+var age = 20;
+console.log(userName, age);
+//we can't assign strings to a variable that is declared number
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(10, 20);
+// addNumbers(10,"20"); This will show error as because 20 is string
+// addNumbers(10); We can't assign only one value in the function too
